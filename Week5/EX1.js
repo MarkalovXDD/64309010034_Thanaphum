7a1cd7dd865044f4763ab8f8eb28101cd28ffaf1
MarkalovXDD <116155766+MarkalovXDD@users.noreply.github.com>
2022-11-17 09:31:04 +0700

    Create EX1.js

---
+++ Week5/EX1.js
@@ -0,0 +1,9 @@
+let obj = {
+    "A4paper" : 120,
+    "FlashDrive" : 130,
+    "Ruler": 25 
+}
+
+console.log("Mr.Yue",obj.A4paper*5,+obj.Ruler*2)
+console.log("Mr.Lee",obj.Flashdrive*2,+obj.A4paper*3)
+console.log("Markalov",obj.A4paper*70+obj.Ruler*50+obj.FlashDrive*80)
