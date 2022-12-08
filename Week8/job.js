430123741aec02a977092563cec4d7178425bc90
MarkalovXDD <116155766+MarkalovXDD@users.noreply.github.com>
2022-12-08 08:18:06 +0700

    Create job.js

---
+++ Week8/job.js
@@ -0,0 +1,66 @@
+let employee = [{
+    "ID":30001,
+    "Name": "นางปรียาดา ตั้งทิพย์",
+    "department": "บุคคล",
+    "position":"HR",
+    "salary":48000,
+    "bonusratinglevel":"A"
+},{
+    "ID":30002,
+    "Name": "นายณัฏฐ์ พงษ์วิโรจน์",
+    "department": "ไอที",
+    "position":"System Engineer",
+    "salary": 57000,
+    "bonusratinglevel":"S"
+},{
+    "ID":30003,
+    "Name": "นายภูวนัย พัฒนปรีชา",
+    "department": "การเงิน",
+    "position":"Account",
+    "salary":38000,
+    "bonusratinglevel":"B"
+},{
+    "ID":30004,
+    "Name": "นางสาวเจนจิรา วงศ์สุรวัฒนา",
+    "department": "บุคคล",
+    "position":"HR",
+    "salary":55000,
+    "bonusratinglevel":"A"
+},{
+    "ID":30005,
+    "Name": "นายพนาวัฒน์ กลิ่นโพธิ์",
+    "department": "บุคคล",
+    "position":"HR",
+    "salary":13000,
+    "bonusratinglevel":"C"
+},{
+    "ID":30006,
+    "Name": "นายธนารีย์ ชัยเจริญ",
+    "department": "การเงิน",
+    "position":"Account",
+    "salary":41000,
+    "bonusratinglevel":"S"
+},{
+    "ID":30007,
+    "Name": "นายนิมมาน สยนานนท์",
+    "department": "บุคคล",
+    "position":"HR",
+    "salary":19000,
+    "bonusratinglevel":"A"
+},{
+    "ID":30008,
+    "Name": "นายกีรติ มากมี",
+    "department": "การเงิน",
+    "position":"Account",
+    "salary":46000,
+    "bonusratinglevel":"C"
+},{
+    "ID":30009,
+    "Name": "นางสาววรรณิศา โตศิลา",
+    "department": "ไอที",
+    "position":"System Engineer",
+    "salary":28000,
+    "bonusratinglevel" : "B"
+}];
+ 
+ 
