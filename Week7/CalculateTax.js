d2815287ac835a69a8c9e2edcc57b98a4cec1165
MarkalovXDD <tanapoompiris@gmail.com>
2023-01-17 10:38:02 +0700

    Create CalculateTax.js

---
+++ Week7/CalculateTax.js
@@ -0,0 +1,118 @@
+let obj=[{
+    "codenumber":"30001",
+    "firstname":"นายปรียาดา",
+    "lastname":"ตั้งทิพย์",
+    "isAlive":true,
+    "address":{
+        "department":"บุลคล",
+        "position":"HR"
+    },
+        "salary":[{
+            "salary":"48,000",
+            "bonuspointslevel":"A"
+        }]
+},{
+    "codenumber":"30002",
+    "firstname":"นายณัฏฐ์",
+    "lastname":"พงษ์วิโรจน์",
+    "isAlive":true,
+    "address":{
+        "department":"ไอที",
+        "position":"System Engineer"
+    },
+        "salary":[{
+            "salary":"57,000",
+            "bonuspointslevel":"S"
+        }]
+},{
+    "codenumber":"30003",
+    "firstname":"นายภูวนัย",
+    "lastname":"พัฒนปรีชา",
+    "isAlive":true,
+    "address":{
+        "department":"การเงิน",
+        "position":"Account"
+    },
+        "salary":[{
+            "salary":"38,000",
+            "bonuspointslevel":"B"
+        }]
+},{
+    "codenumber":"30004",
+    "firstname":"นางสาวเจนวิรา",
+    "lastname":"วงศ์สุรวัฒนา",
+    "isAlive":true,
+    "address":{
+        "department":"บุคคล",
+        "position":"HR"
+    },
+        "salary":[{
+            "salary":"55,000",
+            "bonuspointslevel":"A"
+        }]
+},{
+    "codenumber":"30005",
+    "firstname":"นายพนาวัฒน์",
+    "lastname":"กลิ่นโพธิ์",
+    "isAlive":true,
+    "address":{
+        "department":"บุคคล",
+        "position":"HR"
+    },
+        "salary":[{
+            "salary":"13,000",
+            "bonuspointslevel":"C"
+        }]
+},{
+    "codenumber":"30006",
+    "firstname":"นางธนารีย์",
+    "lastname":"ชัยเจริญ",
+    "isAlive":true,
+    "address":{
+        "department":"การเงิน",
+        "position":"Account"
+    },
+        "salary":[{
+            "salary":"41,000",
+            "bonuspointslevel":"S"
+        }]
+},{
+    "codenumber":"30007",
+    "firstname":"นายนิมมาน",
+    "lastname":"สยนานนท์",
+    "isAlive":true,
+    "address":{
+        "department":"บุคคล",
+        "position":"HR"
+    },
+        "salary":[{
+            "salary":"19,000",
+            "bonuspointslevel":"A"
+        }]
+},{
+    "codenumber":"30008",
+    "firstname":"นายกีรยิ",
+    "lastname":"มากมี",
+    "isAlive":true,
+    "address":{
+        "department":"การเงิน",
+        "position":"Account"
+    },
+        "salary":[{
+            "salary":"46,000",
+            "bonuspointslevel":"C"
+        }]
+},{
+    "codenumber":"30009",
+    "firstname":"นางสาววรรณิศา",
+    "lastname":"โตศิลา",
+    "isAlive":true,
+    "address":{
+        "department":"ไอที",
+        "position":"System Engineer"
+    },
+        "salary":[{
+            "salary":"28,000",
+            "bonuspointslevel":"B"
+        }]
+}];
